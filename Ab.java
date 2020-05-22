dd54a925961bd40407fe2a1bc0c171366e2c28c5
unknown <user@naming-register.com>
2020-05-22 23:42:59 +0530

    it is chair

---
+++ Ab.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Ab
+{
+protected int x=6;
+ public int y=3;
+public static void main(String args[])
+{
+Ab t=new Ab();
+System.out.println(t.x+"  "+t.y);
+}
+}
